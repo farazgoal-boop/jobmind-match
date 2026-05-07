293a30dea98080a10a27fd7b517d520bc5133326
Muhammad Faraz <faragoal@gmail.com>
2026-05-07 14:57:21 +0500

    Fix Android WebView mobile scaling

---
+++ mobile-wrapper/android/app/src/main/java/com/jobmind/match/MainActivity.java
@@ -0,0 +1,23 @@
+package com.jobmind.match;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		WebView webView = getBridge().getWebView();
+		WebSettings settings = webView.getSettings();
+
+		settings.setUseWideViewPort(false);
+		settings.setLoadWithOverviewMode(false);
+		settings.setBuiltInZoomControls(false);
+		settings.setDisplayZoomControls(false);
+		settings.setTextZoom(100);
+	}
+}
